adcef776b19b4baea9fe93df3a1315d19186264c
Paggy <pei-chi.huang@kit.edu>
2021-03-17 19:16:59 +0100

    collection label error

--- app/api/entities/sample_entity.rb
+++ app/api/entities/sample_entity.rb
@@ -90,7 +90,7 @@ module Entities
       cols = object.tag&.taggable_data['collection_labels']&.select do |c|
         c['id'] == ENV['PUBLIC_COLL_ID']&.to_i || c['id'] == ENV['SCHEME_ONLY_REACTIONS_COLL_ID']&.to_i
       end
-      cols.length > 0
+      (cols && cols.length > 0) || false
     end
 
     private
