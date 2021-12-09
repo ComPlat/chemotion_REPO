869169158244db5be2dae7e179c297bdd56c7662
chia-lin.lin <chia-lin.lin@kit.edu>
2021-12-09 22:40:14 +0100

    fix e.split on review page issue

--- app/assets/javascripts/libHome/RepoCommon.js
+++ app/assets/javascripts/libHome/RepoCommon.js
@@ -1845,7 +1845,7 @@ class RenderPublishAnalyses extends Component {
       <Panel key={`analysis-${analysis.id}`} expanded={expanded} className="panel-analyses-public" onToggle={() => { }}>
         <Panel.Heading style={{ border: 'unset' }}>
           <h4><i className="fa fa-area-chart" aria-hidden="true" style={{ fontSize: '1.5em' }} /><b> Published on </b> <i>{formattedTime}</i>
-            {IconLicense(this.props.license, (this.props.publication.author_ids.length > 1))}
+            {IconLicense(null, this.props.license, (this.props.publication.author_ids.length > 1))}
           </h4>
           <p>&nbsp;</p>
           <b>{kind}</b>&nbsp;
