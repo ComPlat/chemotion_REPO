369af52830b67011fcbfa351200935a987c9d6d5
Paggy <pei-chi.huang@kit.edu>
2022-07-19 22:52:38 +0200

    update

--- db/migrate/20220718000000_add_oai_metadata_xml_to_publications.rb
+++ db/migrate/20220718000000_add_oai_metadata_xml_to_publications.rb
@@ -1,16 +1,6 @@
 class AddOaiMetadataXmlToPublications < ActiveRecord::Migration[5.2]
   def self.up
     add_column :publications, :oai_metadata_xml, :text
-
-    err_msg = {}
-    Publication.where(state: ['completed', 'completed ver_20190116155110']).find_each do |pub|
-      begin
-        pub.persit_oai_metadata_xml!
-      rescue StandardError => e
-        err_msg[pub.id] = e.message
-      end
-    end
-    File.write("json_export_oai_metadata_error_#{Time.now.to_i}.json", err_msg.to_json)
   end
 
   def self.down
