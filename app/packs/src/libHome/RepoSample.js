771bd3ad5438ce40054dec7a9eda998c6162aed4
chia-lin.lin <chia-lin.lin@kit.edu>
2023-05-08 23:14:30 +0200

    Resolve "molecular mass descriptor in sample metadata"

--- app/packs/src/libHome/RepoSample.js
+++ app/packs/src/libHome/RepoSample.js
@@ -140,7 +140,6 @@ export default class RepoSample extends Component {
           <span className="repo-pub-title"><IconToMyDB isLogin={isLogin} isPublished={isPublished} id={sample.id} type="sample" /></span>&nbsp;
           <span className="repo-pub-title"><DateInfo isPublished={isPublished} preText="Sample" pubData={pubData} tagData={tagData} /></span>&nbsp;
           <SidToPubChem sid={sample.sid} />&nbsp;
-          <RepoXvialButton isEditable={isReviewer} isLogin={isLogin} allowRequest elementId={sample.id} data={sample.xvial} saveCallback={() => this.updateRepoXvial(sample.molecule_id)} xvialCom={xvialCom} />
           <span className="repo-public-user-comment">
             <PublicCommentModal isReviewer={isReviewer} id={sample.id} type="Sample" title={sample.showed_name} userInfo={userInfo} pageType="molecules" pageId={sample.molecule_id} />&nbsp;
             <UserCommentModal isLogin={isLogin} id={sample.id} type="Sample" title={sample.showed_name} pageType="molecules" pageId={sample.molecule_id} />&nbsp;
@@ -160,7 +159,7 @@ export default class RepoSample extends Component {
         />
         <br />
         <PublicSample {...this.props} embargo={embargo} handleAnalysesLink={this.handleAnalysesLink} handleMaterialLink={this.handleMaterialLink} />
-        <br />
+        <br /><br />
         <div ref={this.materialRef}>
           <MAPanel isEditable={isReviewer} isLogin={isLogin} allowRequest elementId={sample.id} data={sample.xvial} saveCallback={() => this.updateRepoXvial(sample.molecule_id)} xvialCom={xvialCom} />&nbsp;
         </div>
