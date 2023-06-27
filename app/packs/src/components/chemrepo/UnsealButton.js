7580bd4815b78e200f9056c805108d3d383823ee
chia-lin.lin <chia-lin.lin@kit.edu>
2023-06-28 00:04:55 +0200

    add info to lock element function

--- app/packs/src/components/chemrepo/UnsealButton.js
+++ app/packs/src/components/chemrepo/UnsealButton.js
@@ -1,6 +1,7 @@
+/* eslint-disable react/forbid-prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Label, Tooltip, OverlayTrigger } from 'react-bootstrap';
+import { Button, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { permitOn } from '../common/uis';
 import { getElementType } from './publication-utils';
 
@@ -9,30 +10,27 @@ const UnsealBtn = (props) => {
   if (!element) return null;
 
   return !permitOn(element) ? (
-    <OverlayTrigger
-      placement="bottom"
-      overlay={
-        <Tooltip
-          id="_tooltip_unseal_button"
-          className="left_tooltip bs_tooltip"
-        >
-          {`This ${getElementType(
-            element
-          )} has been submitted and is currently locked for
-          modifications. You can 'Unseal' it for editing.`}
-        </Tooltip>
-      }
-    >
-      <div style={{ display: 'inline-block' }}>
-        <div onClick={fnUnseal}>
-          <span className="collection-label repo-btn-success" key={element.id}>
-            <Label>
-              <i className="fa fa-unlock" aria-hidden="true" />
-            </Label>
-          </span>
-        </div>
-      </div>
-    </OverlayTrigger>
+    <>
+      <OverlayTrigger
+        placement="bottom"
+        overlay={
+          <Tooltip
+            id="_tooltip_unseal_button"
+            className="left_tooltip bs_tooltip"
+          >
+            {`This ${getElementType(
+              element
+            )} has been submitted and is currently locked for
+          modifications. Click to 'Unseal' it for editing.`}
+          </Tooltip>
+        }
+      >
+        <Button bsSize="xsmall" bsStyle="success" onClick={fnUnseal}>
+          <i className="fa fa-lock" aria-hidden="true" />
+          &nbsp; Locked
+        </Button>
+      </OverlayTrigger>
+    </>
   ) : null;
 };
 
