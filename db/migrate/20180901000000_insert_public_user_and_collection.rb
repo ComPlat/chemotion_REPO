16e0554031540b193e24eabc2d817f7707cda6bd
Jochen Klar <mail@jochenklar.de>
2023-07-13 13:16:15 +0200

    Add a migration to create "public user"

---
+++ db/migrate/20180901000000_insert_public_user_and_collection.rb
@@ -0,0 +1,23 @@
+class InsertPublicUserAndCollection < ActiveRecord::Migration[4.2]
+  class User < ActiveRecord::Base
+    self.inheritance_column = nil
+    devise :database_authenticatable
+  end
+
+  def change
+    attributes = {
+      email: 'public.user@eln.edu',
+      first_name: 'Public',
+      last_name: 'User',
+      password: 'PleaseChangeYourPassword',
+      name_abbreviation: 'PUB',
+    }
+    user = User.create!(attributes)
+    user.update!(account_active: true) if column_exists?(:users, :account_active)
+    user.update!(confirmed_at: DateTime.now) if column_exists?(:users, :account_active)
+
+    Profile.create!(user_id: user.id)
+
+    Collection.find_or_create_by(user_id: user.id, label: 'chemotion')
+  end
+end
