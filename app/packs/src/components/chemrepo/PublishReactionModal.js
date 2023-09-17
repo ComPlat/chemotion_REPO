69e5e003133ddc095ed56f9ce0ea5ebbbda4cfda
Chia-Lin Lin <chia-lin.lin@kit.edu>
2023-09-17 20:29:40 +0000

    Resolve "make properties invisible on publication page"

--- app/packs/src/components/chemrepo/PublishReactionModal.js
+++ app/packs/src/components/chemrepo/PublishReactionModal.js
@@ -865,7 +865,6 @@ export default class PublishReactionModal extends Component {
               >
                 <span>Skip amount and yield validation (the product has no amount and yield)</span>
               </Checkbox>
-              {awareEmbargo}
               <PanelGroup accordion id={`panelgroup_${reaction.id}`} defaultActiveKey={0}>
                 <Panel
                   eventKey="2"
