b0e53cd199b16f4c428ed866b4fb36dfc21e3daa
Adam Basha <55552142+adambasha0@users.noreply.github.com>
2023-11-16 08:54:43 +0100

    feat: include chemicals with import and export of collections (#1604)

--- lib/import/import_collections.rb
+++ lib/import/import_collections.rb
@@ -88,6 +88,7 @@ module Import
         gate_collection if @gt == true
         import_collections if @gt == false
         import_samples
+        import_chemicals if @gt == false
         import_residues
         ## import_elemental_compositions if @gt == true
         import_reactions
@@ -197,6 +198,21 @@ module Import
       end
     end
 
+    def import_chemicals
+      @data.fetch('Chemical', {}).each do |_uuid, fields|
+        sample = @instances.fetch('Sample').fetch(fields.fetch('sample_id'))
+        next unless sample
+
+        chemical = Chemical.create!(fields.slice(
+          'cas',
+          'chemical_data',
+        ).merge(
+          sample_id: sample&.id,
+        ))
+        chemical.save!
+      end
+    end
+
     def import_samples
       @data.fetch('Sample', {}).each do |uuid, fields|
         # look for the molecule_name
@@ -249,6 +265,7 @@ module Import
           'decoupled',
           'molecular_mass',
           'sum_formula',
+          'inventory_sample',
         ).merge(
           created_by: @current_user_id,
           collections: fetch_many(
