aeff164b5169ff0d411d177919bee33815810b9b
Jochen Klar <mail@jochenklar.de>
2023-12-08 16:25:48 +0100

    Fix assign_sample_to_collections

--- app/models/concerns/reaction_sample_collections.rb
+++ app/models/concerns/reaction_sample_collections.rb
@@ -9,7 +9,14 @@ private
 
   def assign_sample_to_collections
     self.reaction.collections.each do |c|
-      CollectionsSample.where(sample: self.sample, collection: c).first_or_create
+      # same as CollectionsSample.where(sample: self.sample, collection: c).first_or_create
+      # but should work with act_as_paranoid
+      collection_sample = CollectionsSample.where(sample: self.sample, collection: c).with_deleted.first
+      if collection_sample.nil?
+        CollectionsSample(sample: self.sample, collection: c).save!
+      elsif collection_sample.deleted?
+        collection_sample.restore
+      end
     end
   end
 end
